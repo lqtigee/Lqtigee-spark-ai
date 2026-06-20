ff40f9c66557c29e5a8fb7ed403375d228b05967
lqtigee <57350307+lqtigee@users.noreply.github.com>
2026-06-20 12:37:01 +0900

    Add process output pump skeleton

---
+++ src/main/java/com/lqtigee/sparkai/runtime/ProcessOutputPump.java
@@ -0,0 +1,8 @@
+package com.lqtigee.sparkai.runtime;
+
+public class ProcessOutputPump {
+
+    public void attach(String runId, ManagedProcess process) {
+        throw new UnsupportedOperationException("Process output pump is not implemented yet");
+    }
+}
