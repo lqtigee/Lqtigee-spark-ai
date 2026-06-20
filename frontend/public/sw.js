96aec063931560975bc63452f6bf9557b4dfc274
lqtigee <57350307+lqtigee@users.noreply.github.com>
2026-06-20 14:16:29 +0900

    Add frontend service worker

---
+++ frontend/public/sw.js
@@ -0,0 +1,31 @@
+const STATIC_CACHE_NAME = "lqtigee-static-v1";
+const STATIC_SHELL_URLS = ["/", "/manifest.webmanifest", "/icons/icon-192.png", "/icons/icon-512.png"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(STATIC_CACHE_NAME).then((cache) => cache.addAll(STATIC_SHELL_URLS)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) =>
+        Promise.all(
+          cacheNames
+            .filter((cacheName) => cacheName !== STATIC_CACHE_NAME)
+            .map((cacheName) => caches.delete(cacheName))
+        )
+      )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+
+  if (url.pathname.startsWith("/api/")) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
+  event.respondWith(caches.match(event.request).then((cachedResponse) => cachedResponse ?? fetch(event.request)));
+});
