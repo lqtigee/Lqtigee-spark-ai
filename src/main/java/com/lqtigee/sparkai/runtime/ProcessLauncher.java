dc39fda6791a8b8bbee650096e689a9e378b921e
lqtigee <57350307+lqtigee@users.noreply.github.com>
2026-06-20 12:29:15 +0900

    Add process launcher skeleton

---
+++ src/main/java/com/lqtigee/sparkai/runtime/ProcessLauncher.java
@@ -0,0 +1,8 @@
+package com.lqtigee.sparkai.runtime;
+
+public class ProcessLauncher {
+
+    public ManagedProcess start(String runId, CommandSpec spec) {
+        throw new UnsupportedOperationException("Process launcher is not implemented yet");
+    }
+}
