7171988003aca180a2c7e4bbb42756b7dcf77527
lqtigee <57350307+lqtigee@users.noreply.github.com>
2026-06-21 16:55:14 +0900

    Add attachment error codes

--- src/test/java/com/lqtigee/sparkai/error/ErrorCodeTest.java
+++ src/test/java/com/lqtigee/sparkai/error/ErrorCodeTest.java
@@ -12,4 +12,15 @@ class ErrorCodeTest {
         assertThat(ErrorCode.valueOf("OPENCODE_AGENT_OUTPUT_INVALID")).isEqualTo(ErrorCode.OPENCODE_AGENT_OUTPUT_INVALID);
         assertThat(ErrorCode.valueOf("OPENCODE_AGENT_SOURCE_UNAVAILABLE")).isEqualTo(ErrorCode.OPENCODE_AGENT_SOURCE_UNAVAILABLE);
     }
+
+    @Test
+    void containsAttachmentCodes() {
+        assertThat(ErrorCode.valueOf("ATTACHMENT_MISSING")).isEqualTo(ErrorCode.ATTACHMENT_MISSING);
+        assertThat(ErrorCode.valueOf("ATTACHMENT_TOO_LARGE")).isEqualTo(ErrorCode.ATTACHMENT_TOO_LARGE);
+        assertThat(ErrorCode.valueOf("ATTACHMENT_CONTENT_TYPE_FORBIDDEN")).isEqualTo(ErrorCode.ATTACHMENT_CONTENT_TYPE_FORBIDDEN);
+        assertThat(ErrorCode.valueOf("ATTACHMENT_STORAGE_FAILED")).isEqualTo(ErrorCode.ATTACHMENT_STORAGE_FAILED);
+        assertThat(ErrorCode.valueOf("ATTACHMENT_NOT_FOUND")).isEqualTo(ErrorCode.ATTACHMENT_NOT_FOUND);
+        assertThat(ErrorCode.valueOf("ATTACHMENT_DELETE_FAILED")).isEqualTo(ErrorCode.ATTACHMENT_DELETE_FAILED);
+        assertThat(ErrorCode.valueOf("ATTACHMENT_PATH_INVALID")).isEqualTo(ErrorCode.ATTACHMENT_PATH_INVALID);
+    }
 }
